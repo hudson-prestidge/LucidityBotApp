a7c01669a0065ec4b21ab33ef2ace0e53d68f6e7
Hudson Prestidge <charcon@ihug.co.nz>
2016-08-22 22:37:26 +1200

    install underscore, refactor mostusedword

--- data/db.js
+++ data/db.js
@@ -1,45 +1,31 @@
 var config = require('../knexfile').development
 var knex = require('knex')(config)
+var _ = require('underscore')
+
+// getMostUsedWord().then(function(arr){console.log(arr);})
 
 function getMostUsedWord() {
   return knex.select('text')
     .from('messages')
     .then(function(data){
-      // data is in the form of an array with [{text:''},{text:''}]
-      var wordArray = data.map(function(obj){
-       return obj.text
-     }).join(' ').split(' ')
-      var wordFrequency = {}
-
-      wordArray.map(function(word) {
-        if(wordFrequency[word]){
-          wordFrequency[word] += 1
-        } else {
-          wordFrequency[word] = 1
-        }
-      })
-
-      var mostUsedWord = ''
-      var mostWordUses = 0
-
-      Object.keys(wordFrequency).map(function(key){
-        if(wordFrequency[key] > mostWordUses){
-          mostUsedWord = key
-          mostWordUses = wordFrequency[key]
-        }
-      })
-      return mostUsedWord
+      return _.chain(data)
+        .reduce(function(m, r){return m.concat(r.text.split(' '))}, [])
+        .countBy(function(e){return e.toLowerCase()})
+        .pairs()
+        .sortBy(function(a){return -a[1]})
+        .first(5)
+        .value()
     })
 }
 
 function getMostAnnoyingUsers(){
   return knex.select('user_id')
-        .count('*')
-        .from('messages')
-        .where('text', 'like', '%day9tv%')
-        .groupBy('user_id')
-        .orderBy('count', 'desc')
-        .limit(5)
+    .count('*')
+    .from('messages')
+    .where('text', 'like', '%day9tv%')
+    .groupBy('user_id')
+    .orderBy('count', 'desc')
+    .limit(5)
 }
 
 function getMostActiveUserIds() {
