fe5e39f74d9a8b018992886493e4d414d6613c41
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-13 16:30:53 +1300

    server and app file split correctly

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require('express')
-var users = require('./routes/users')
-var words = require('./routes/words')
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy
 var cookieParser = require('cookie-parser')
@@ -9,56 +7,110 @@ var bodyParser = require('body-parser')
 var morgan = require('morgan')
 var flash = require('connect-flash')
 var bcrypt = require('bcrypt-nodejs')
+var http = require('http')
+var path = require('path')
+var pug = require('pug')
+
+var config = require('./knexfile').development
+var knex = require('knex')(config)
+
+var users = require('./server/routes/users')
+var commands = require('./server/routes/commands')
+var words = require('./server/routes/words')
 
 var app = express()
+var server = http.createServer(app)
 
-// passport.use('signup', new LocalStrategy(
-//   function(name, password, done) {
-//     if (name != '' && password != '') {
-//       knex('users').where('name', name).then(function(users){
-//         if(users.length > 0) {
-//           return done(null, false, { message: 'Username is already taken.' })
-//         } else {
-//           knex('users').returning('id').insert({
-//             name: name,
-//             password: bcrypt.hashSync(password)
-//           }).then(function(newIds){
-//             knex('users').where('id', newIds[0]).first().then(function(user){
-//               return done(null, user)
-//             })
-//           })
-//         }
-//       })
-//     } else {
-//       return done(null, false, { message: 'Username and password must be provided details.' })
-//     }
-//   }
-// ))
+app.set('views', path.join(__dirname, './server/views'))
+app.set('view engine', 'pug');
 
-// app.use(express.static('public'))
-// app.use(morgan('dev'))
-// app.use(cookieParser())
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(session({ cookie: { maxAge: 60000 }, secret: 'bodyparseristheworstthing', resave: false, saveUninitialized: false }))
-// app.use(passport.initialize())
-// app.use(passport.session())
-// app.use(flash())
-// app.use(bodyParser.json())
-//
-// app.use('/api/v1/users/', users)
-// app.use('/api/v1/commands/', commands)
-// app.use('/api/v1/words/', words)
-// //
-// // app.get('/login', function(req, res) {
-// //   res.render('login')
-// // })
-// //
-// // app.get('*', function (req, res) {
-// //   res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// // })
-//
-//
-// app.use('/api/v1/users', users)
-// app.use('/api/v1/words', words)
+app.use(express.static('public'))
+
+app.use('/api/v1/users/', users)
+app.use('/api/v1/commands/', commands)
+app.use('/api/v1/words/', words)
+
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ cookie: { maxAge: 60000 }, secret: 'bodyparseristheworstthing', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(bodyParser.json())
+
+passport.serializeUser(function(user, callback) {
+  callback(null, user.id)
+})
+
+passport.deserializeUser(function(id, callback) {
+  knex('users').where('id', id).first().then(function(user) {
+    callback(null, user)
+  })
+})
+
+const isAuthenticated = function(req, res, next) {
+  if (req.isAuthenticated()) return next()
+
+  req.flash('error', 'You must be logged in to access that page.')
+  res.redirect('/login')
+}
+
+
+passport.use('login', new LocalStrategy(
+  function(name, password, done) {
+    knex('users').where('name', name).first().then(function(user){
+      if(user) {
+        // if(bcrypt.compareSync(password, user.password)) {
+        if(password === user.password) {
+          return done(null, user)
+        } else {
+          return done(null, false, { message: 'Invalid password.' })
+        }
+      } else {
+        return done(null, false, { message: 'Incorrect details.' })
+      }
+    })
+  }
+))
+
+app.get('/login', function(req, res) {
+  res.render('login', { messages: req.flash() })
+})
+
+app.post('/login',
+  passport.authenticate('login', { failureRedirect: '/login', failureFlash: true } ),
+  function(req, res) {
+    res.redirect('/')
+  }
+)
+
+app.get('/logout',
+  function(req, res){
+    req.logout()
+    res.redirect('/login')
+  }
+)
+
+app.get('/commands', isAuthenticated, function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'app.html'))
+})
+
+app.get('/users', isAuthenticated, function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'app.html'))
+})
+
+app.get('/stats', isAuthenticated, function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'app.html'))
+})
+
+app.get('/home', isAuthenticated, function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'app.html'))
+})
+
+app.get('/', isAuthenticated, function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'app.html'))
+})
+
 
 module.exports = app
