b8dd4a50f53ea2a1b0d5dbe5fd0940c2d2c1c94a
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-26 23:02:22 +1300

    remove commented out unused code

--- client/components/commands/regularCommands.js
+++ client/components/commands/regularCommands.js
@@ -1,95 +1,3 @@
-// import React from 'react'
-// import $ from 'jquery'
-// import { Link } from 'react-router'
-// import Command from './command'
-// import CommandNavbar from './CommandNavbar'
-//
-// export default class RegularCommands extends React.Component {
-//
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//
-//     }
-//   }
-//
-//   clickHandler(id) {
-//     var self = this
-//     $.ajax({
-//       method: 'DELETE',
-//       url: '/api/v1/commands/' + id,
-//       success: (data) => self.forceUpdate()
-//     })
-//   }
-//
-//   componentDidMount(){
-//     var self = this
-//     $.ajax({
-//       method: 'get',
-//       url: '/api/v1/commands/regularCommands',
-//       success: function(data){
-//         var commands = data.map(function(d){
-//           return{
-//           name: d.name,
-//           response: d.response,
-//           id: d.id,
-//           linkString: '/commands/' + d.id
-//           }
-//         })
-//         self.setState({commands: commands })
-//       }
-//     })
-//   }
-//
-//   componentDidMount(){
-//     var self = this
-//     $.ajax({
-//       method: 'get',
-//       url: '/api/v1/commands/regularCommands',
-//       success: function(data){
-//         var commands = data.map(function(d){
-//           return{
-//           name: d.name,
-//           response: d.response,
-//           id: d.id,
-//           linkString: '/commands/' + d.id
-//           }
-//         })
-//         self.setState({commands: commands })
-//       }
-//     })
-//   }
-//
-//   render () {
-//     return (
-//       <div>
-//           <h1> List of Bot Commands </h1>
-//           <Link className='col-md-2 col-md-offset-5 addbutton' to='/commands/new' > Add New Command </Link>
-//           <table>
-//             <thead>
-//               <tr>
-//                 <th> Command </th>
-//                 <th> Response </th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {this.state.commands ?
-//                  this.state.commands.map((command, i) =>
-//                   <tr key={i}>
-//                     <td className='command'>{command.name}</td>
-//                     <td>{command.response}</td>
-//                     <td><Link to={command.linkString}> Edit Command </Link></td>
-//                     <td><button onClick={() => this.clickHandler(command.id)}> Delete Command </button></td>
-//                   </tr>)
-//                 : null}
-//             </tbody>
-//           </table>
-//         <Link className='col-md-2 col-md-offset-5 addbutton' to='/commands/new' > Add New Command </Link>
-//       </div>
-//     )
-//   }
-// }
-
 import React from 'react'
 import request from 'superagent'
 import { Link } from 'react-router'
@@ -203,9 +111,6 @@ export default class RegularCommands extends React.Component {
         : null
       }
       </div>
-
-
-
     )
   }
 }
